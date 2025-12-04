2c837a646e68bdcea687bb319561700d71fbd8ab
jaimecabrito01 <azevedocarneirojaimevictor@gmail.com>
2025-12-04 16:40:39 -0300

    first project files

---
+++ api/java-jato/src/main/java/com/api/java_jato/JavaJatoApplication.java
@@ -0,0 +1,13 @@
+package com.api.java_jato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJatoApplication.class, args);
+	}
+
+}
---
+++ api/java-jato/src/test/java/com/api/java_jato/JavaJatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.java_jato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
