75859617fb212abec3c10818052168eefb6e3032
jaimecabrito01 <azevedocarneirojaimevictor@gmail.com>
2025-12-08 20:05:48 -0300

    fix: rename repository

--- api/java-jato/src/main/java/com/api/java_jato/repository/ServiceTypeRepository.java
+++ api/java-jato/src/main/java/com/api/java_jato/repository/ServiceTypeRepository.java
@@ -2,8 +2,8 @@ package com.api.java_jato.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import com.api.java_jato.models.ServiceType;
 @Repository
-public interface ServiceType extends JpaRepository<ServiceType,Long> {
+public interface ServiceTypeRepository extends JpaRepository<ServiceType,Long> {
     
 }
